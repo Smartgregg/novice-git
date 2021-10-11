35f239d2d5d865039cad2d74de17841353505bd5
= <=>
2021-10-11 15:14:15 +0100

    senior man

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+	<form action="">
+		<p>Name: First name <input type="text"></p>
+		<p>Name: Last  name <input type="text"></p>
+	</form>
+
+</body>
+</html> 
